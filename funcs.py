a9ab2e0118e09bd0adf2c672881383060e0d2f2c
Jeffrey Knutsen <jknutsen92@gmail.com>
2022-07-17 16:02:04 -0700

    Refactored local directory init logic

--- funcs.py
+++ funcs.py
@@ -6,16 +6,15 @@ import shutil
 def load_db_from_external_cache(local_path: str, external_path:str, db_name:str) -> None:
     local = Path(f"{local_path}{db_name}")
     extern = Path(f"{external_path}{db_name}")
-    if extern.exists():
-        if not local.exists() or local.stat().st_mtime < extern.stat().st_mtime:
-            local_dir = Path(local_path)
-            if not local_dir.exists():
-                local_dir.parent.mkdir()
-            shutil.copyfile(extern, local)
-    elif not local.exists():
+
+    if not local.exists():
         local.parent.mkdir()
         local.touch()
 
+    if extern.exists():
+        if not local.exists() or local.stat().st_mtime < extern.stat().st_mtime:
+            shutil.copyfile(extern, local)
+        
 
 def load_db_from_cloud():
     # TODO
