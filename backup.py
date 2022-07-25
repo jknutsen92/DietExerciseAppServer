c1bcf3838b38aa453e22c219b3b4574c103c8017
Jeffrey Knutsen <jknutsen92@gmail.com>
2022-07-25 15:26:42 -0700

    Completed export db to google drive

--- backup.py
+++ backup.py
@@ -1,9 +1,11 @@
 from cfg                    import *
 from pathlib                import Path
 from Google                 import Create_Service
-from googleapiclient.http   import MediaFileUpload
+from googleapiclient.http   import MediaFileUpload, MediaIoBaseDownload
 from googleapiclient.errors import HttpError
 from shutil                 import copyfile
+from datetime               import datetime
+from io                     import BytesIO
 
 def load_db_from_external_cache(local_path: str, external_path:str, db_name:str) -> None:
     local = Path(f"{local_path}{db_name}")
@@ -18,14 +20,36 @@ def load_db_from_external_cache(local_path: str, external_path:str, db_name:str)
             copyfile(extern, local)
         
 
-def load_db_from_cloud():
+def load_db_from_cloud(local_path, db_name):
+    local = Path(f"{local_path}{db_name}")
     service = Create_Service(G_CLIENT_SECRET_FILE, G_API_NAME, G_API_VERSION, G_SCOPES)
 
-    response = service.files().list(q={
-        "name":     f"{G_DRIVE_FOLDER}/{DATABASE_NAME}"         
-    }).execute()
+    try:
+        response = service.files().list(q="name='databases'", fields="files(id)").execute()
+        folder_id = response.get("files")[0]["id"]
 
-    print(response)
+        query = f"name='{DATABASE_NAME}' and '{folder_id}' in parents"
+        response = service.files().list(q=query, fields="files(id, modifiedTime)").execute()
+        file = response.get("files")[0]
+        # TODO: fix datetime string format
+        # timestring format: '2022-07-25T21:58:23.278Z'
+        dt = datetime.strptime(file["modifiedTime"], "Y-m-d")
+        ts = dt.timestamp()
+        lcl_ts = local.stat().st_mtime
+        if ts > lcl_ts:
+            request = service.files().get_media(fileId=file["id"])
+            db = BytesIO()
+            downloader = MediaIoBaseDownload(db, request)
+            done = False
+            while not done:
+                status, done = downloader.next_chunk()
+                print(f"{DATABASE_NAME} download {int(status.progress() * 100)}% completed")
+
+            copyfile(db, local)
+
+    except HttpError as e:
+        print(f"Could not find {DATABASE_NAME} in drive")
+        print(e)
     
 
 def write_db_to_external_cache(local_path: str, external_path: str, db_name: str) -> None:
@@ -45,11 +69,31 @@ def write_db_to_cloud(local_path, db_name):
     local = Path(f"{local_path}{db_name}")
     service = Create_Service(G_CLIENT_SECRET_FILE, G_API_NAME, G_API_VERSION, G_SCOPES)
 
-    file_metadata = { "name": f"{db_name}", "folder": G_DRIVE_FOLDER }
+    folder_meta = {
+        "name":     "databases",
+        "mimeType": "application/vnd.google-apps.folder"
+    }
+    try:
+        response = service.files().list(q="name = 'databases'", fields="files(id)").execute()
+        folder_id = response.get("files")[0]["id"]
+    except HttpError as e:
+        print(e)
+        response = service.files().create(body=folder_meta, fields="id").execute()
+        folder_id = response.get("id")
+
+
+    file_metadata = { "name": f"{db_name}", "parents": [folder_id] }
     media = MediaFileUpload(local)
+    
+    query = f"name='{db_name}' and '{folder_id}' in parents"
+    response = service.files().list(q=query, fields="files(id)").execute()
+    files = response.get("files")
+    if files:
+        service.files().update(body=file_metadata, media_body=media, fileId=files[0]["id"])
+    else:
+        service.files().create(body=file_metadata, media_body=media).execute()
+    
 
-    service.files().create(body=file_metadata, media_body=media).execute()
 
-
-#load_db_from_cloud()
-write_db_to_cloud("databases/", "store.db")
+load_db_from_cloud("databases/", "store.db")
+#write_db_to_cloud("databases/", "store.db")
